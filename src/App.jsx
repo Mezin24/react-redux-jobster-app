e1432d6af63dcca905fb6169805ac9fd1297f4a5
Pavel Mezencev <mezencev24@mail.ru>
2023-03-20 11:53:14 +0300

    add register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import { Lading, Register, Dashboard, ErrorPage } from './pages/';
 import { Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path='/'>
-        <Route index element={<Lading />} />
-        <Route path='register' element={<Register />} />
-        <Route path='dashboard' element={<Dashboard />} />
-        <Route path='*' element={<ErrorPage />} />
-      </Route>
-    </Routes>
+    <>
+      <Routes>
+        <Route path='/'>
+          <Route index element={<Lading />} />
+          <Route path='register' element={<Register />} />
+          <Route path='dashboard' element={<Dashboard />} />
+          <Route path='*' element={<ErrorPage />} />
+        </Route>
+      </Routes>
+      <ToastContainer position='top-center' autoClose={3000} />
+    </>
   );
 };
 export default App;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,79 @@
+import { useState, useEffect } from 'react';
+import { Logo, FormRow } from '../components';
+import Wrapper from '../assets/wrappers/RegisterPage';
+import { toast } from 'react-toastify';
+
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  isMember: true,
+};
+
 const Register = () => {
-  return <div>Register</div>;
+  const [values, setValues] = useState(initialState);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const { name, email, password, isMember } = values;
+    if (
+      (name.trim() === '' && !isMember) ||
+      email.trim() === '' ||
+      password.trim() === ''
+    ) {
+      toast.error('Please enter a value');
+      return;
+    }
+    console.log(values);
+  };
+
+  const toggleMember = () => {
+    setValues((prev) => ({ ...prev, isMember: !prev.isMember }));
+  };
+
+  return (
+    <Wrapper className='full-page'>
+      <form className='form' onSubmit={onSubmit}>
+        <Logo />
+        <h3>{values.isMember ? 'Login' : 'Register'}</h3>
+        {/* name field */}
+
+        {!values.isMember && (
+          <FormRow
+            type='text'
+            name='name'
+            value={values.name}
+            handleChange={handleChange}
+          />
+        )}
+        <FormRow
+          type='text'
+          name='email'
+          value={values.email}
+          handleChange={handleChange}
+        />
+        <FormRow
+          name='password'
+          type='password'
+          value={values.password}
+          handleChange={handleChange}
+        />
+        <button type='submit' className='btn btn-block'>
+          submit
+        </button>
+        <p>
+          {values.isMember ? 'Not a member yet?' : 'Already a member?'}
+          <button onClick={toggleMember} type='button' className='member-btn'>
+            {values.isMember ? 'Register' : 'Login'}
+          </button>
+        </p>
+      </form>
+    </Wrapper>
+  );
 };
 export default Register;
